dfe55836249ffe29f2de27acae91ab140934efe8
hasibdurjoy <hrd2571@gmail.com>
2021-09-26 01:54:19 +0600

    second update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import Bands from './Components/Bands/Bands';
 
 function App() {
   return (
-    <div className="concert-container">
-      <div className="bands-container">
-        <Bands></Bands>
-      </div>
-      <div className="cart-container">
-        <h4>My Cart</h4>
-      </div>
+    <div>
+      <Bands></Bands>
     </div>
   );
 }
--- src/Components/Bands/Bands.js
+++ src/Components/Bands/Bands.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Band from '../Band/Band';
-
+import './Bands.css'
 const Bands = () => {
     const [bands, setBands] = useState([]);
     useEffect(() => {
@@ -9,11 +9,19 @@ const Bands = () => {
             .then(data => setBands(data))
     }, []);
     return (
-        <div className="bands-container container row row-cols-1 row-cols-md-3 g-4">
-            {
-                bands.map(band => <Band key={band.id} band={band}></Band>)
-            }
+        <div className="concert-container">
+            <div className="bands-container">
+                <div className="bands-container container row row-cols-1 row-cols-md-3 g-4">
+                    {
+                        bands.map(band => <Band key={band.id} band={band}></Band>)
+                    }
+                </div>
+            </div>
+            <div className="cart-container">
+                <h4>My Cart</h4>
+            </div>
         </div>
+
     );
 };
 
